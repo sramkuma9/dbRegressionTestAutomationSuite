23546bdc5d4fe92f13d06909d0e327f7f85703f4
Srini <ajay.rsin@gmail.com>
2019-08-12 16:03:57 +0530

    Created PageObjects for Login Page.

---
+++ src/test/java/db/endToEndTests/pageFactory/LoginPage.java
@@ -0,0 +1,42 @@
+package db.endToEndTests.pageFactory;
+
+import db.endToEndTests.utils.ReadPropertyFile;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+    public static WebDriver driver;
+    public WebDriverWait wait;
+    private ReadPropertyFile readPropertyFile = new ReadPropertyFile();
+
+    @FindBy(css =  "#app > div > div > div.styles__AppContent-cDnHLr.gXPOuk > div > div.styles__ContentContainer-kcVUrs.DjEAQ > section.styles__Container-ieicpK.iVPZSq > div.styles__Container-jGsLTA.fXXdOL > form.styles__StyledForm-feCSaU.hJcysH > div:nth-child(1) > div > div > div > input")
+    WebElement bniUserName;
+
+    @FindBy(css =  "#app > div > div > div.styles__AppContent-cDnHLr.gXPOuk > div > div.styles__ContentContainer-kcVUrs.DjEAQ > section.styles__Container-ieicpK.iVPZSq > div.styles__Container-jGsLTA.fXXdOL > form.styles__StyledForm-feCSaU.hJcysH > div:nth-child(2) > div > div > div > input")
+    WebElement bniPassword;
+
+    @FindBy(css =  "#app > div > div > div.styles__AppContent-cDnHLr.gXPOuk > div > div.styles__ContentContainer-kcVUrs.DjEAQ > section.styles__Container-ieicpK.iVPZSq > div.styles__Container-jGsLTA.fXXdOL > form.styles__StyledForm-feCSaU.hJcysH > div.styles__ButtonContainer-iUsGiN.gqPErL > div > button > span.jss29")
+    WebElement bniSignIn;
+
+    public LoginPage(WebDriver driver) {
+        LoginPage.driver = driver;
+        AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(driver, 100);
+        //This initElements method will create all WebElements
+        PageFactory.initElements(factory, this);
+        wait = new WebDriverWait(driver, 5);
+    }
+
+    public void enterBniUserName(){
+        bniUserName.sendKeys(readPropertyFile.loadAndReadPropertyFile("bniUsername"));
+    }
+    public void enterBniPassword(){
+        bniUserName.sendKeys(readPropertyFile.loadAndReadPropertyFile("bniPassword"));
+    }
+    public void clickBniSignInButton(){
+        bniSignIn.click();
+    }
+}
