7ae77e047abae27e86dd33299d1375750899d897
Srini <ajay.rsin@gmail.com>
2019-08-13 13:21:20 +0530

    Timeout increased.

--- src/test/java/db/endToEndTests/bniCommonLibraries/SignOut.java
+++ src/test/java/db/endToEndTests/bniCommonLibraries/SignOut.java
@@ -7,12 +7,12 @@ public class SignOut {
     private db.endToEndTests.pageFactory.BNIConnect bniConnect;
 
     public void signOutBni() throws Exception{
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
         bniConnect = new db.endToEndTests.pageFactory.BNIConnect(driver);
         bniConnect.hoverOnOptions();
-        TimeUnit.SECONDS.sleep(3);
+        TimeUnit.SECONDS.sleep(5);
         bniConnect.clickSignOut();
-        TimeUnit.SECONDS.sleep(3);
+        TimeUnit.SECONDS.sleep(5);
         driver.quit();
     }
 }
